ed8f03b16b8bd1c9993a6c7997a12f52aa4c9a3c
Dishit79 <dishit79@gmail.com  git config --global user.name Nawafit config --global user.email dishit79@gmail.com>
2025-01-19 01:18:06 -0500

    web

--- websocket.js
+++ websocket.js
@@ -1,19 +1,37 @@
-var WebSocketConnector = function(url) {
-	this.socket = new WebSocket(url);
-	this.socket.onmessage = function(event) {
-		console.log(event.data);
-	};
-	this.socket.onopen = function(event) {
-		console.log("connected");
-	};
-	this.socket.onclose = function(event) {
-		console.log("disconnected");
-	};
-	this.socket.onerror = function(event) {
-		console.log("error");
-	};
-};
+url = "ws://127.0.0.1:8000/ws"
+// url = "wss://echo.websocket.org/"
+var ws, playerID, playerList = [];
 
-WebSocketConnector.prototype.send = function(data) {
-	this.socket.send(data);
-};
+ws = new WebSocket(url)
+ws.onopen = function() {
+	console.log("WebSocket connection opened")
+	ws.send("Hello, WebSocket!")
+}
+
+ws.onmessage = function(event) {
+	console.log(event.data)
+}
+
+ws.onclose = function() {
+	console.log("WebSocket connection closed")
+}
+
+ws.onerror = function(event) {
+	console.log("WebSocket error")
+}
+
+// send me the data in a json format 
+// ws.send(JSON.stringify({ "name": "John", "age": 30 })); 
+
+setInterval(function() {
+	ws.send("gaze");
+}, 1000);
+
+
+// setInterval(function() {
+// 	ws.send("emotion");
+// }, 1000);
+
+// setInterval(function() {
+// 	ws.send("gaze");
+// }, 1000);
